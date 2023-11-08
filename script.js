2457b4edd60f9ce73f4f639255cf5a4532c69f3c
arkhaledwebdev <arkhaledwebdev@gmail.com>
2023-11-08 22:44:50 +0200

    still setting up script

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const gameBoard = (function(){
+const gameBoard = (function () {
 
     let board = [];
     let columns = 3;
@@ -10,13 +10,88 @@ const gameBoard = (function(){
         board[i] = [];
 
         for (let j = 0; j < columns; j++) {
-            board[i].push(createCell({i,j}))
+            board[i].push(createCell(i * columns + j))
         }
-        
+
     }
+
+    const markCell = (cellNo, player) => {
+
+        console.log(board.find(c => c.cellNo === cellNo));
+
+    }
+
+    const getBoard = () => board;
+
+    return { getBoard, markCell };
+})();
+
+const gameController = (function (p1_name, p2_name) {
+
+    const players = [
+        { playerName: p1_name, mark: 'X' },
+        { playerName: p2_name, mark: 'O' }];
+
+    let currentPlayer = players[0];
+
+    const switchTurn = () => {
+        if (currentPlayer === players[0]) {
+            currentPlayer = players[1];
+        }
+        else {
+            currentPlayer = players[0];
+        }
+    }
+
+    const getCurrentPlayer = () => currentPlayer;
+
+    return { getCurrentPlayer, switchTurn };
+
 })();
 
 
-const createCell = function (rowNo,columnNo,mark){
-    
+function playRound() {
+
+    let board = gameBoard.getBoard();
+
+    const currentPlayer = gameController.getCurrentPlayer();
+
+    // emulate p1 move
+
+    gameBoard.markCell(4, currentPlayer);
+
+    console.log(board);
+
+    gameController.switchTurn();
+
+    gameBoard.markCell(2, currentPlayer);
+
+    console.log(board);
+
 }
+
+function createCell(cellNo) {
+
+    let isMarked = false;
+
+    let mark = '';
+
+    let player = '';
+
+    const setMark = (mark, player) => {
+        this.isMarked = true;
+        this.mark = mark;
+        this.player = player;
+    };
+
+    const getMark = () => mark;
+
+    const getPlayer = () => player;
+
+    return { cellNo, isMarked, setMark, getMark, getPlayer };
+}
+
+playRound();
+
+
+
